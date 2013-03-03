89e1a2fcff0909b9cf6f0ec94cb3df9edb363c36
Kris Markel <kris@blackpixel.com>
2013-03-03 19:08:29 +0100

    Add database updater and project changes.

---
+++ Preloading/XcodePics/XcodePics/DatabaseUpdater/ROBKDatabaseUpdater.h
@@ -0,0 +1,15 @@
+//
+//  ROBKDatabaseUpdater.h
+//  XcodePics
+//
+//  Created by Kris Markel on 3/3/13.
+//  Copyright (c) 2013 RobotK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ROBKDatabaseUpdater : NSObject
+
++ (void) loadJSONFromURL:(NSURL *)JSONURL;
+
+@end
---
+++ Preloading/XcodePics/XcodePics/DatabaseUpdater/ROBKDatabaseUpdater.m
@@ -0,0 +1,20 @@
+//
+//  ROBKDatabaseUpdater.m
+//  XcodePics
+//
+//  Created by Kris Markel on 3/3/13.
+//  Copyright (c) 2013 RobotK. All rights reserved.
+//
+
+#import "ROBKDatabaseUpdater.h"
+
+#import "ESJSONOperation.h"
+
+@implementation ROBKDatabaseUpdater
+
++ (void) loadJSONFromURL:(NSURL *)JSONURL
+{
+    
+}
+
+@end

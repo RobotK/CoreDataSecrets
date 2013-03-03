12a115e7ca839affcfa5468d8711dbbd57407c92
Kris Markel <kris@blackpixel.com>
2013-03-03 19:24:43 +0100

    Initialize the core data stack.

--- Preloading/XcodePics/XcodePics/ROBKAppDelegate.m
+++ Preloading/XcodePics/XcodePics/ROBKAppDelegate.m
@@ -8,14 +8,23 @@
 
 #import "ROBKAppDelegate.h"
 
+#import "DCTCoreDataStack.h"
+
+@interface ROBKAppDelegate ()
+
+@property (nonatomic, strong) DCTCoreDataStack *coreDataStack;
+
+@end
+
 @implementation ROBKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+	self.coreDataStack = [[DCTCoreDataStack alloc] initWithStoreFilename:@"XcodePics"];
+
+	return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.

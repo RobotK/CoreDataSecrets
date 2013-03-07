361f4a59e2db923dd589b2837547556f350585ef
Kris Markel <kris@blackpixel.com>
2013-03-07 14:40:28 +0100

    Add the initial version of the core data coordinator.

---
+++ DeleteDatabase/XcodePics/CoreDataManager/ROBKCoreDataCoordinator.h
@@ -0,0 +1,71 @@
+//
+//  ROBKCoreDataCoordinator.h
+//  XcodePicsWithDelete
+//
+//  Created by Kris Markel on 3/7/13.
+//  Copyright (c) 2013 RobotK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ Based on the ideas and original implementaion of Rob Rix (@rob_rix) and Jonathan Schwarz (@jschwarz), but converted to use an operation queue rather than GCD so we can cancel tasks.
+
+ This class is used to manage concurrency for Core Data related operations.
+
+ All core data work (basically anything that involves an NSManagedObjectContext) should be done using one of the coordinate* methods, unless it's happening on the main thread.
+
+ Update Notifications:
+
+ If a component needs to be aware when some data has changed, it can listen for the ROBKCoordinatorDataUpdateNotification notification. (This is typically necessary for view controllers that need to redisplay when the underlying data has changed.) The notification's userInfo dictionary keys are described in more detail below, but in general, you need to examine the list of changed objects (using ROBKCoordinatorChangedObjectsKey), and if there's a change you're interested in, update your UI appropriately.
+
+ If you're running in the main thread, the MOC returned by -[ROBKCoreDataCoordinator mainThreadContext] will already have the updated data merged in. If you're operating outside of the main thread, you'll want to get updated data using -[ROBKCoreDataCoordinator coordinateReadingWithBlock:].
+
+ */
+
+/** This notification is fired after any changes have been persisted. The keys for the notification's userInfo dictionary are detailed below. DO NOT rely on this notification being posted on a particular queue or thread. */
+extern NSString * const ROBKCoordinatorDataUpdateNotification;
+
+/** This returns a set that contains every object that's been inserted, deleted, or updated by the latest core data save. It's useful to determine if there have been any changes to a particular entity type that you're class is interensted in. */
+extern NSString * const ROBKCoordinatorChangedObjectsKey;
+
+/** This returns the userInfo dictionary from the original NSManagedObjectContextDidSaveNotification. Details can be found in the NSManagedObjectContext class reference. This is useful if you need to know if particular objects have been specifically inserted, deleted, or changed. */
+extern NSString * const ROBKCoordinatorOriginalNotificationUserInfoKey;
+
+typedef void(^ROBKCoreDataCoordinatorBlock)(NSManagedObjectContext *context, NSOperation *operation);
+
+@interface ROBKCoreDataCoordinator : NSObject
+
++ (instancetype) sharedCoordinator;
+
+/**
+
+ Retuns a managed object context for use on the main thread. In debug mode, it will crash (assert) if called on anything other than the main thread.
+
+ */
+- (NSManagedObjectContext *)mainThreadContext;
+
+/**
+
+ Coordinates an asynchronous background read
+
+ Use this method for read-only core data tasks that aren't being done on the main thread. Ideally, all such operations should be done using this method.
+
+ This will run concurrently with other reads, but will block writes until it completes.
+
+ */
+-(void)coordinateReadingWithBlock:(ROBKCoreDataCoordinatorBlock)block;
+
+/**
+
+ Coordinates an asynchronous background write.
+
+ Use this method for any core data tasks that require persiting the results that aren't being done on the main thread. Ideally, all such operations will be done using this method.
+
+ This will be run serially with respect to both reads and writes.
+
+ */
+-(void)coordinateWritingWithBlock:(ROBKCoreDataCoordinatorBlock)block;
+
+
+@end
---
+++ DeleteDatabase/XcodePics/CoreDataManager/ROBKCoreDataCoordinator.m
@@ -0,0 +1,168 @@
+//
+//  ROBKCoreDataCoordinator.m
+//  XcodePicsWithDelete
+//
+//  Created by Kris Markel on 3/7/13.
+//  Copyright (c) 2013 RobotK. All rights reserved.
+//
+
+#import "XCodePicsCoreDataLibrary/DCTCoreDataStack+ROBKAdditions.h"
+
+#import "ROBKCoreDataCoordinator.h"
+
+NSString * const ROBKCoordinatorDataUpdateNotification = @"ROBKCoordinatorDataUpdateNotification";
+NSString * const ROBKCoordinatorChangedObjectsKey = @"ROBKCoordinatorChangedObjectsKey";
+NSString * const ROBKCoordinatorOriginalNotificationUserInfoKey = @"ROBKCoordinatorOriginalNotificationUserInfoKey";
+
+@interface ROBKCoreDataCoordinator ()
+
+@property (nonatomic, strong) NSOperationQueue *coreDataOperationQueue;
+@property (nonatomic, strong) DCTCoreDataStack *coreDataStack;
+
+@end
+
+@implementation ROBKCoreDataCoordinator
+
++ (instancetype) sharedCoordinator
+{
+    static ROBKCoreDataCoordinator *s_sharedInstance;
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        s_sharedInstance = [ROBKCoreDataCoordinator new];
+    });
+
+    return s_sharedInstance;
+}
+
+#pragma mark - Object Lifecycle
+
+- (id) init
+{
+    self = [super init];
+
+    if (self) {
+        _coreDataOperationQueue = [NSOperationQueue new];
+        [_coreDataOperationQueue setName:@"ROBKCoreDataCoordinator CoreDataOperationQueue"];
+
+        _coreDataStack = [DCTCoreDataStack sharedCoreDataStack];
+    }
+
+    return self;
+}
+
+- (void) dealloc
+{
+    [_coreDataOperationQueue cancelAllOperations];
+}
+
+#pragma mark - Core Data helpers
+
+- (NSManagedObjectContext *) mainThreadContext
+{
+	 NSAssert([NSThread isMainThread], @"This must be called on the main thread.");
+    return self.coreDataStack.managedObjectContext;
+}
+
+#pragma mark - Coordinated reads and writes
+
+- (void) coordinateReadingWithBlock:(ROBKCoreDataCoordinatorBlock)block
+{
+    NSBlockOperation *coordinatedReadOperation = [NSBlockOperation new];
+
+    // Make a weak reference to avoid a retain cycle.
+    __weak NSBlockOperation *weakReadOperation = coordinatedReadOperation;
+
+    [coordinatedReadOperation addExecutionBlock:^{
+
+        @autoreleasepool {
+            __strong NSBlockOperation *strongReadOperation = weakReadOperation;
+            if (!strongReadOperation) {
+                return;
+            }
+
+            if (strongReadOperation.isCancelled) {
+                return;
+            }
+
+            NSManagedObjectContext *context = [NSManagedObjectContext new];
+				context.persistentStoreCoordinator = self.coreDataStack.persistentStoreCoordinator;
+				context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy;
+
+#ifdef DEBUG
+				// Watch for saves occuring during read-only operations and assert if we find one.
+				id observer = [[NSNotificationCenter defaultCenter] addObserverForName:NSManagedObjectContextDidSaveNotification object:context queue:nil usingBlock:^(NSNotification *note) {
+					 NSCAssert(FALSE, @"We should not be saving during a read operation.");
+				}];
+#endif
+
+				block(context, strongReadOperation);
+
+#ifdef DEBUG
+				[[NSNotificationCenter defaultCenter] removeObserver:observer];
+#endif
+
+        }
+
+    }];
+
+    [self.coreDataOperationQueue addOperation:coordinatedReadOperation];
+}
+
+- (void) coordinateWritingWithBlock:(ROBKCoreDataCoordinatorBlock)block
+{
+    NSBlockOperation *coordinatedWriteOperation = [NSBlockOperation new];
+
+    // Make a weak reference to avoid a retain cycle.
+    __weak NSBlockOperation *weakWriteOperation = coordinatedWriteOperation;
+
+    [coordinatedWriteOperation addExecutionBlock:^{
+
+        @autoreleasepool {
+            __strong NSBlockOperation *strongWriteOperation = weakWriteOperation;
+            if (!strongWriteOperation) {
+                return;
+            }
+
+            if (strongWriteOperation.isCancelled) {
+                return;
+            }
+
+            NSManagedObjectContext *context = [NSManagedObjectContext new];
+				context.persistentStoreCoordinator = self.coreDataStack.persistentStoreCoordinator;
+				context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
+
+				id observation = [[NSNotificationCenter defaultCenter] addObserverForName:NSManagedObjectContextDidSaveNotification object:context queue:nil usingBlock:^(NSNotification *note) {
+					 dispatch_sync(dispatch_get_main_queue(), ^{
+						  @autoreleasepool {
+								[[self mainThreadContext] mergeChangesFromContextDidSaveNotification:note];
+
+								NSDictionary *originalUserInfo = [note userInfo];
+								NSMutableSet *changedObjectsSet = [NSMutableSet setWithSet:[originalUserInfo objectForKey:NSInsertedObjectsKey]];
+								[changedObjectsSet addObjectsFromArray:[[originalUserInfo objectForKey:NSUpdatedObjectsKey] allObjects]];
+								[changedObjectsSet addObjectsFromArray:[[originalUserInfo objectForKey:NSDeletedObjectsKey] allObjects]];
+
+								NSDictionary *userInfo = @{ROBKCoordinatorOriginalNotificationUserInfoKey : originalUserInfo, ROBKCoordinatorChangedObjectsKey: changedObjectsSet};
+
+								[[NSNotificationCenter defaultCenter] postNotificationName:ROBKCoordinatorDataUpdateNotification object:[self mainThreadContext] userInfo:userInfo];
+						  }
+					 });
+				}];
+
+				block(context, strongWriteOperation);
+
+				[[NSNotificationCenter defaultCenter] removeObserver:observation];
+        }
+
+    }];
+
+    // This may block for a while as we wait for other operations to complete, so we want to make sure we're not on the main thread.
+    NSAssert(![NSThread isMainThread], @"We're probably blocking the main thread!");
+
+    // Wait for other operations to finish.
+    [self.coreDataOperationQueue waitUntilAllOperationsAreFinished];
+	 
+    [self.coreDataOperationQueue addOperation:coordinatedWriteOperation];
+}
+
+@end
